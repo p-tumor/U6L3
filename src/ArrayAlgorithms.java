9a92d090b3f6bf88bf8821f9d90850beef56f0e0
BT_4N2_24 <stu@gmail.com>
2023-01-05 12:32:46 -0500

    addded stuff fr

---
+++ src/ArrayAlgorithms.java
@@ -0,0 +1,106 @@
+public class ArrayAlgorithms {
+    /** Prints each String in wordList, on its own line, followed by “!”.
+     *  Does NOT mutate (modify) wordList.
+     *  PRECONDITION: wordList.length > 0
+     *
+     *  @param wordList  original array of Strings; does not get modified
+     */
+    public static void printExclamation(String[] wordList)
+    { /* implement this method! */ }
+
+    /** Adds a “!” to the end of each String in wordList (does not print).
+     *  DOES mutate (modify) original wordList.
+     *  PRECONDITION: wordList.length > 0
+     *
+     *  @param wordList  original array of Strings, modified by method
+     */
+    public static void addExclamation(String[] wordList)
+    { /* implement this method! */ }
+
+    /** Returns the sum of all values in numList.
+     *  Does NOT mutate (modify) numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  array of integers to find sum
+     *  @return  the sum of all numbers in the list
+     */
+    public static int sum(int[] numList)
+    { /* implement this method! */ }
+
+    /** Returns the average of all values in numList, as a double.
+     *  Does NOT mutate (modify) numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  array of integers to find average
+     *  @return  the average of all numbers in the list
+     *
+     *  Use your sum method for this! (don’t rewrite that logic!)
+     */
+    public static double average(int[] numList)
+    { /* implement this method! */ }
+
+    /** Returns the value in numList that represents the minimum value in numList.
+     *  Does NOT mutate (modify) numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  array of integers to find minimum
+     *  @return  the minimum value in the array
+     */
+    public static int minimum(int[] numList)
+    { /* implement this method! */ }
+
+    /** Returns the value in numList that represents the maximum value in numList.
+     *  Does NOT mutate (modify) numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  array of integers to find maximum
+     *  @return  the maximum value in the array
+     */
+    public static int maximum(int[] numList)
+    { /* implement this method! */ }
+
+    /** Multiplies each number in numList by multiplier.
+     *  DOES mutate (modify) original numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of integers, modified by method
+     *  @param multiplier  number to multiply each element in numList
+     */
+    public static void multiplyBy(int[] numList, int multiplier)
+    { /* implement this method! */ }
+
+    /** Returns a NEW array containing the squares of the elements in the original
+     *  numList array, in the same position.
+     *  Does NOT mutate (modify) original numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of integers to be squared
+     *  @return  new array containing squares of the values in numList
+     */
+    public static int[] squares(int[] numList)
+    { /* implement this method! */ }
+
+    /** Switches each boolean value in boolList to its opposite value; true values
+     *  becomes false, and false values become true.
+     *  DOES mutate (modify) original boolList.
+     *  PRECONDITION: boolList.length > 0
+     *
+     *  @param numList  original array of booleans, modified by method
+     */
+    public static void flipBool(boolean[] boolList)
+    { /* implement this method! */ }
+
+    /** Returns a String representing the array of ints as a printable String,
+     *  including open and closing brackets, with values separated by commas.
+     *  This method should function identically to Arrays.toString()!
+
+     *  Does NOT mutate (modify) original numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of ints to be represented in String form
+     */
+    public static String customToString(int[] numList)
+    { /* implement this method! */ }
+
+
+}
